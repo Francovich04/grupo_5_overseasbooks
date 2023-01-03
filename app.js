26111f2336a4affdf7029d79af36e40cd61d39dd
juanamayorga <juanamayorga2001@gmail.com>
2023-01-03 10:58:55 -0300

    levanto servidor

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const publicPath = path.resolve(__dirname, './public')
+
+app.use(express.static(publicPath))
+
+app.listen (3001, () => console.log ('Servidor corriendo'));
+
+app.get('/shoppingcart', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/shoppingCart.html'))
+})
