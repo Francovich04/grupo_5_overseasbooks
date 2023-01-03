cf21abc27a563afd0f087ef4fa4e46697c8f9cac
juanamayorga <juanamayorga2001@gmail.com>
2023-01-03 16:23:20 -0300

    agrego archivo shoppingcart2(sin envio) y levanto servidor

--- app.js
+++ app.js
@@ -10,3 +10,7 @@ app.listen (3001, () => console.log ('Servidor corriendo'));
 app.get('/shoppingcart', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/shoppingCart.html'))
 })
+
+app.get('/shoppingcart2', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/shoppingCart2.html'))
+})
