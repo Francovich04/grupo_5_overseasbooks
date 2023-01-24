5c637c5921100aec2a7f36f9d013c43802cb8ad5
uleesays <uleesays@gmail.com>
2023-01-24 18:47:37 -0300

    creo database libros.js

--- src/database/libros.js
+++ src/database/libros.js
@@ -1,11 +1,248 @@
 let libros = [
+
     {
-    title: "Harry Potter", 
-    author: "J.K Rowling",
-    category: "Ciencia Ficción",
-    price: 2000,
-    img: 'https://styles.redditmedia.com/t5_3a9l7/styles/communityIcon_3eaeqzl4bo551.png'
-    }
+        id: 1,
+
+        titleEng: "The Secret Messenger",
+
+        titleEsp: "El Mensajero Secreto",
+
+        author: "Mandy Robotham",
+
+        category: "Best Sellers",
+
+        price: 9.99,
+
+        img: "https://m.media-amazon.com/images/I/51fcXmG6g9L.jpg"
+    },
+
+    {
+        id: 2,
+
+        titleEng: "Babel",
+
+        titleEsp: "Babel",
+
+        author: "R. F. Kuang",
+
+        category: "Best Sellers",
+
+        price: 14.99,
+
+        img: "https://m.media-amazon.com/images/I/51016lbT2fL._AC_SY780_.jpg"
+    },
+
+    {
+        id: 3,
+
+        titleEng: "Lessons in Chemistry",
+
+        titleEsp: "Lecciones en Química",
+
+        author: "Bonnie Garmus",
+
+        category: "Best Sellers",
+
+        price: 8.99,
+
+        img: "https://m.media-amazon.com/images/I/41sjsd8kD7L._AC_SY780_.jpg"
+    },
+
+    {
+        id: 4,
+
+        titleEng: "The Dark Stuff",
+
+        titleEsp: "La Cosa Oscura",
+
+        author: "Paul Seiple",
+
+        category: "Best Sellers",
+
+        price: 5.99,
+
+        img: "https://m.media-amazon.com/images/I/41sjsd8kD7L._AC_SY780_.jpg"
+    },
+
+    {
+        id: 5,
+
+        titleEng: "Air Magic",
+
+        titleEsp: "Aire Magia",
+
+        author: "Astrea Taylor",
+
+        category: "Best Sellers",
+
+        price: 21.99,
+
+        img: "https://m.media-amazon.com/images/I/51koyd-uMhL.jpg"
+    },
+
+    {
+        id: 6,
+
+        titleEng: "An Immense World",
+
+        titleEsp: "Un Mundo Inmenso",
+
+        author: "Ed Yong",
+
+        category: "Science",
+
+        price: 12.99,
+
+        img: "https://m.media-amazon.com/images/I/51koyd-uMhL.jpg"
+    },
+
+    {
+        id: 7,
+
+        titleEng: "How the world really works",
+
+        titleEsp: "Como Funciona el Mundo",
+
+        author: "Vaclav Smil",
+
+        category: "Science",
+
+        price: 11.99,
+
+        img: "https://m.media-amazon.com/images/I/41+MkmjAu0L._AC_SY780_.jpg"
+    },
+
+    {
+        id: 8,
+
+        titleEng: "Stolen Focus",
+
+        titleEsp: "Foco Robado",
+
+        author: "Johann Hari",
+
+        category: "Science",
+
+        price: 19.99,
+
+        img: "https://images-na.ssl-images-amazon.com/images/I/41JkN0wefOL._AC_SX368_.jpg"
+    },
+
+    {
+        id: 9,
+
+        titleEng: "Breath",
+
+        titleEsp: "Aliento",
+
+        author: "James Nestor",
+
+        category: "Science",
+
+        price: 9.99,
+
+        img: "https://m.media-amazon.com/images/I/413cAh3xCfL._AC_SY780_.jpg"
+    },
+
+    {
+        id: 10,
+
+        titleEng: "The Song of the Cell",
+
+        titleEsp: "La Canción Celular",
+
+        author: "Siddhartha Mukherjee",
+
+        category: "Science",
+
+        price: 13.99,
+
+        img: "https://images-na.ssl-images-amazon.com/images/I/519CPWnyTIL._AC_SX368_.jpg"
+    },
+
+    {
+        id: 11,
+
+        titleEng: "Fairy Tale",
+
+        titleEsp: "Cuento de Hadas",
+
+        author: "Stephen King",
+
+        category: "Ficción",
+
+        price: 34.99,
+
+        img: "https://m.media-amazon.com/images/I/51ECRZXoGyL._AC_SY780_.jpg"
+    },
+
+    {
+        id: 12,
+
+        titleEng: "Bright Creatures",
+
+        titleEsp: "Criaturas Inteligentes",
+
+        author: "Shelby Van Pelt",
+
+        category: "Ficción",
+
+        price: 29.99,
+
+        img: "https://m.media-amazon.com/images/I/51fMaH1tRyL._AC_SY780_.jpg"
+    },
+
+    {
+        id: 13,
+
+        titleEng: "All Rights Reserved",
+
+        titleEsp: "Derechos Reservados",
+
+        author: "Gregory Scott Katsoulis",
+
+        category: "Ficción",
+
+        price: 19.99,
+
+        img: "https://m.media-amazon.com/images/I/51cYLMrs79L._AC_SY780_.jpg"
+    },
+
+    {
+        id: 14,
+
+        titleEng: "Book of Night",
+
+        titleEsp: "Libro de la Noche",
+
+        author: "Holly Black",
+
+        category: "Ficción",
+
+        price: 25.99,
+
+        img: "https://m.media-amazon.com/images/I/51+WaMJkznL._AC_SY780_.jpg"
+    },
+
+    {
+        id: 15,
+
+        titleEng: "The Maid",
+
+        titleEsp: "La criada",
+
+        author: "Nita Prose",
+
+        category: "Ficción",
+
+        price: 28.99,
+
+        img: "https://m.media-amazon.com/images/I/41PzatfkdOL._AC_SY780_.jpg"
+    },
+
+
+
+
 ];
 
 
