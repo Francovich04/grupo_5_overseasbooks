b0b06510636ad4022a949535cb4c47fcd684cda4
uleesays <uleesays@gmail.com>
2023-02-17 16:11:36 -0300

    agrego multer a middlewares y validaciones dedicada

--- src/routes/products.js
+++ src/routes/products.js
@@ -8,57 +8,16 @@ const { create,
     detailsById, 
     database, 
     editView } = require('../controllers/productsControllers');
-const multer = require('multer');
-const path = require('path');
-
-const { body } = require('express-validator');
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../../public/images/products'))
-    },
-    filename: (req, file, cb) => {
-        const newFile = `/product-${Date.now()}_img${path.extname(file.originalname)}`
-        cb(null, newFile)
-    }
-});
-
-const upload = multer({ storage });
-
-const validations = [
-    body('titleEng').notEmpty().withMessage('Escribí el titulo en Inglés'),
-    body('titleEsp').notEmpty().withMessage('Escribí el titulo en Español'),
-    body('color').notEmpty().withMessage('Selecioná un color de encabezado de tarjeta'),
-    body('author').notEmpty().withMessage('Escribí un autor'),
-    body('category').notEmpty().withMessage('Seleciona una categoría'),
-    body('price').notEmpty().withMessage('Escribí un precio válido'),
-    body('img').custom((value, { req }) => {
-        let file = req.file;
-        let validExtensions = ['.jpg', '.jpeg', '.png'];
-
-        if (!file) {
-            throw new Error('Debes subir una imagen');
-        } else {
-            let fileExtension = path.extname(file.originalname);
-            if (!validExtensions.includes(fileExtension)) {
-
-                throw new Error(`Los formatos válidos son ${validExtensions.join(', ')}`);
-
-            }
-
-        }
-
-        return true;
-    })
-]
+const multerMiddleware = require('../middlewares/multerMiddleware');
+const validations = require('../validations/allValidations');
 
 
 routerProducts.get('/products/create', create);
-routerProducts.post('/products/create', upload.single('img'), validations, addBook);
+routerProducts.post('/products/create', multerMiddleware.productsUpload.single('img'), validations.productValidations, addBook);
 
 routerProducts.get('/products/edit/', editView)
 routerProducts.get('/products/edit/:id', edit);
-routerProducts.put('/products/edit/:id', upload.single('img'), editConfirm);
+routerProducts.put('/products/edit/:id', multerMiddleware.productsUpload.single('img'), editConfirm);
 
 routerProducts.delete('/products/edit/delete/:id', deleteBook)
 
