47d02b900f6fd6a7724397b6103cfa3402164982
uleesays <uleesays@gmail.com>
2023-03-06 23:49:01 -0300

    arreglo users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -9,19 +9,23 @@ const { login,
     userLogout } = require('../controllers/userControllers');
 const authMiddleware = require('../middlewares/authMiddleware');
 const userLoggedMiddleware = require('../middlewares/userLoggedMiddleware');
+const userLoggedMiddleware = require('../middlewares/userLoggedMiddleware');
 
 const guestMiddleware = require('../middlewares/guestMiddleware');
 const multerMiddleware = require('../middlewares/multerMiddleware');
 const validations = require('../validations/allValidations');
 const adminMiddleware = require('../middlewares/adminMiddleware');
+const adminMiddleware = require('../middlewares/adminMiddleware');
 
 routerLoginRegister.get('/user/login', guestMiddleware, login);
+routerLoginRegister.post('/user/login', validations.emailValidations, userLoggedMiddleware, processLogin);
 routerLoginRegister.post('/user/login', validations.emailValidations, userLoggedMiddleware, processLogin);
 routerLoginRegister.get('/user/register', guestMiddleware, register);
 routerLoginRegister.post('/user/register', multerMiddleware.usersUpload.single('avatar'), validations.userValidations, processRegister);
 routerLoginRegister.get('/user/passwordreset', passwordreset);
-routerLoginRegister.get('/user/profile', userProfile);
-routerLoginRegister.get('/user/logout', userLogout);
+routerLoginRegister.get('/user/profile', adminMiddleware, userProfile );
+routerLoginRegister.get('/user/logout', userLogout)
+
 
 
 module.exports = routerLoginRegister;
