42ff24afe8b7d1dd970837cd589396145f036988
DavidArce397 <darce397@gmail.com>
2023-03-29 23:23:09 -0300

    modelos sequelize

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -5,6 +5,75 @@ let fs = require('fs');
 /* let booksJSON = fs.readFileSync(path.join(__dirname,'../data/libros.json')); */
 
 
+let db = require('../../grupo_5_overseasbooks/models');
+const Op = db.Sequelize.Op;
+
+
+let mainControllers = {
+    listAuthors: (req, res) => {
+        db.Author.findAll(
+            {
+                order: [
+                    ['id', "DESC"]
+                ],
+                // limit : 100
+            }
+        )
+            .then(function (autores) {
+                res.render(path.join(__dirname, '../views/vistaPruebaAutores'), { autores: autores });
+            })
+    },
+
+    listBooks : (req, res) => {
+        db.Book.findAll(
+            {
+                include: [
+                    {association: "categories"},
+                    {association: "authors"}
+                    ],
+
+                order: [
+                    ['id', "DESC"]
+                ],
+                // limit : 100
+            }
+        )
+            .then(function (books) {
+                res.render(path.join(__dirname, '../views/vistaPruebaAutores'), { books });
+            })
+    },
+
+    listCategories : (req, res) => {
+        db.Category.findAll(
+            {
+                order: [
+                    ['id', "DESC"]
+                ]
+                // limit : 100
+            }
+        )
+            .then(function (categories) {
+                res.render(path.join(__dirname, '../views/vistaPruebaAutores'), { categories });
+            })
+        },
+
+        listUsers : (req, res) => {
+            db.User.findAll(
+                {
+                    order: [
+                        ['id', "DESC"]
+                    ]
+                    // limit : 100
+                }
+            )
+                .then(function (users) {
+                    res.render(path.join(__dirname, '../views/vistaPruebaAutores'), { users });
+                })
+            },
+    
+};
+
+
 const main = (req, res) => {
     
     let booksJSON = fs.readFileSync(path.join(__dirname,'../data/libros.json'));
@@ -17,6 +86,6 @@ const main = (req, res) => {
 }
 
 
-module.exports = {main};
+module.exports = {main, mainControllers};
 
     
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const routerMain = express.Router();
-const {main} = require('../controllers/mainControllers');
+const {main, mainControllers} = require('../controllers/mainControllers');
+
 
 routerMain.get('/', main );
+
+routerMain.get('/autores', mainControllers.listBooks);
 
 module.exports = routerMain;
