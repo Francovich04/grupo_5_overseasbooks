092df6fe3709061008e5d71c388d25f2177fee68
uleesays <uleesays@gmail.com>
2023-05-23 16:49:18 -0300

    reparo funcion js para ocultar footers

--- public/js/js-footer.js
+++ public/js/js-footer.js
@@ -7,23 +7,35 @@ function hide() {
 }
 
 function pressButton(event) {
-    hide()
+    
     let target = event.target.id;
+    
+
+    if (target != '' && target != 'quienesSomos' && target != 'trabajaConNosotros' && target != 'contactoFooter' && target != 'terminos' && target != 'cookiesExplained') {
+        hide()
+    }
+    
     if (target == "quienes") {
         document.getElementById("quienesSomos").style.display = "grid"; 
+        
     } else if (target == "trabajar") {
         document.getElementById("trabajaConNosotros").style.display = "grid"; 
         window.scrollTo(0,2800);
+        
     } else if (target == "contactanos") {
         document.getElementById("contactoFooter").style.display = "grid"; 
         window.scrollTo(0,2800);
+        
     } else if (target == "terminosYcondi") {
         document.getElementById("terminos").style.display = "grid"; 
         window.scrollTo(0,2800);
+        
     }  else if (target == "cookiesFooter") {
         document.getElementById("cookiesExplained").style.display = "grid"; 
         window.scrollTo(0,2800);
+       
     }
+   
 }
 
 
