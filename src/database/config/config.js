2f6fcb1ae213c795b944dc6715449e4c916fc262
DavidArce397 <darce397@gmail.com>
2023-05-27 21:15:20 -0300

    modifico archivo config.js prueba

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -6,16 +6,16 @@ module.exports = {
     "database": process.env.DATABASE,
     "host": process.env.DBHOST,
     "dialect": process.env.DIALECT
-  }
-// pruebaaaa
+  },
+  // pruebaaaa
 
-  // "test": {
-  //   "username": "root",
-  //   "password": "root",
-  //   "database": "mydb",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // },
+  "development": {
+    "username": process.env.DBUSER,
+    "password": process.env.DBPASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.DBHOST,
+    "dialect": process.env.DIALECT
+  }
   // "production": {
   //   "username": "root",
   //   "password": "root",
