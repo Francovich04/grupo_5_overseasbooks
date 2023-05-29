1faa6238b9b7d449fb855070448bcb77c373ad54
DavidArce397 <darce397@gmail.com>
2023-05-29 00:17:14 -0300

    modifico archivo config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,24 +1,48 @@
-
 module.exports = {
-  "development": {
-    "username": process.env.DBUSER,
-    "password": process.env.DBPASSWORD,
-    "database": process.env.DATABASE,
-    "host": process.env.DBHOST,
-    "dialect": process.env.DIALECT
+  development: {
+    username: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DBHOST,
+    dialect: process.env.DIALECT
   },
-  // pruebaaaa
+  test: {
+    username: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DBHOST,
+    dialect: process.env.DIALECT
+  },
+  production: {
+    username: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DBHOST,
+    dialect: process.env.DIALECT
+  }
+};
 
-  "test": {
-    "username": process.env.DBUSER,
-    "password": process.env.DBPASSWORD,
-    "database": process.env.DATABASE,
-    "host": process.env.DBHOST,
-    "dialect": process.env.DIALECT
-  },
-  "username": process.env.DBUSER,
-  "password": process.env.DBPASSWORD,
-  "database": process.env.DATABASE,
-  "host": process.env.DBHOST,
-  "dialect": process.env.DIALECT
-}
+
+// module.exports = {
+//   "development": {
+//     "username": process.env.DBUSER,
+//     "password": process.env.DBPASSWORD,
+//     "database": process.env.DATABASE,
+//     "host": process.env.DBHOST,
+//     "dialect": process.env.DIALECT
+//   },
+//   // pruebaaaa
+
+//   "test": {
+//     "username": process.env.DBUSER,
+//     "password": process.env.DBPASSWORD,
+//     "database": process.env.DATABASE,
+//     "host": process.env.DBHOST,
+//     "dialect": process.env.DIALECT
+//   },
+//   "username": process.env.DBUSER,
+//   "password": process.env.DBPASSWORD,
+//   "database": process.env.DATABASE,
+//   "host": process.env.DBHOST,
+//   "dialect": process.env.DIALECT
+// }
