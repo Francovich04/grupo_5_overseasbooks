0a8afa532bf171ca1bb9bc100532433cf37e7909
DavidArce397 <darce397@gmail.com>
2023-05-29 00:11:19 -0300

    modifico index.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -15,12 +15,10 @@ module.exports = {
     "database": process.env.DATABASE,
     "host": process.env.DBHOST,
     "dialect": process.env.DIALECT
-  }
-  // "production": {
-  //   "username": "root",
-  //   "password": "root",
-  //   "database": "mydb",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // }
+  },
+  "username": process.env.DBUSER,
+  "password": process.env.DBPASSWORD,
+  "database": process.env.DATABASE,
+  "host": process.env.DBHOST,
+  "dialect": process.env.DIALECT
 }
